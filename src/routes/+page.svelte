a51d78f8c674f2179e6cecc9817f9153820c384b
Oleh <dudkoleg@gmail.com>
2025-01-27 11:46:10 +0200

    added page with meta data and ssr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,20 @@
-<script>
+<script lang="ts">
 	import HomeSlider from '@/components/HomePage/HomeSlider.svelte';
 	import TopCategories from '@/components/HomePage/TopCategories.svelte';
 	import TopItems from '@/components/HomePage/TopItems.svelte';
+
+	export let data: { title: string; description: string } = {
+		title: 'Default Title',
+		description: 'Default Description'
+	};
 </script>
+
+<svelte:head>
+	<title>{data.title}</title>
+	<meta name="description" content={data.description} />
+	<meta property="og:title" content={data.title} />
+	<meta property="og:description" content={data.description} />
+</svelte:head>
 
 <div>
 	<HomeSlider />
---
+++ src/routes/+page.ts
@@ -0,0 +1,9 @@
+export const ssr = true;
+
+export function load() {
+	return {
+		title: 'Boo Clone - придбати БУ товари вигідно!',
+		description:
+			'Boo Clone - найкраща платформа для покупки БУ товарів в Україні вигідно. Завітай та обирай серед надійних продавців!'
+	};
+}
